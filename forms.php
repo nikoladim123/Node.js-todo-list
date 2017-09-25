3c17f746ceb8b7ecded5ea60f3e89f2278ec6381
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2017-09-25 14:31:52 +0200

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action="testsave.php" method="post">
+<input type="text" name="nametest"/><br/>
+<input type="password" name="passtest"/><br/>
+<input type="submit" value="log"/>
+</form>
+</body>
+</html>
